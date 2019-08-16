2bba35375532e2538bd85b114a3d00936a2d50b1
blittable <blittable-bits@gmail.com>
2019-08-16 16:23:44 -0600

    lesson_one template

---
+++ lesson_one/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let name = "Nook";
+    let name_with_description = add_description(name); 
+
+    println!("Name with description: {:?}" xxxxx);
+}
+
+fn add_description(name: xxxxxx) -> xxxxx {
+
+    //add "is great!" to the name 
+}
