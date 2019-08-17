ae9894e577ee6e43f3467c3b643676fdc912d954
blittable <blittable-bits@gmail.com>
2019-08-17 00:22:13 -0600

    trim back

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -3,19 +3,5 @@ fn main() {
 
     let name = "Nook";
     let name_with_description = add_description(&name); 
-    let result = add();
-
-    println!("Name with description: {:?}", name_with_description);
-    println!("Name with description: {:?}", result); 
 }
-
-fn add_description(name: &str) -> String {
-    [name, " is great"].concat()
-}
-
-
-fn return_something() -> i32 {
-    1
-}
-
 
