1ca29dff3c943b150af990f16e725945cd5b2fc0
khem <khem@mycostech.com>
2019-08-17 14:18:32 +0700

    complete lesson one

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-
-    let name = "Nook";
+    let name = String::from("Nook");
     let name_with_description = add_description(name); 
 
-    println!("Name with description: {:?}" xxxxx);
+    println!("hello world");
+    println!("Name with description: {}", name_with_description);
 }
 
-fn add_description(name: xxxxxx) -> xxxxx {
-
-    //add "is great!" to the name 
+// Take ownership from caller, update, and then give back ownership to caller
+fn add_description(mut name: String) -> String {
+    name.push_str(" is great!");
+    name
 }
