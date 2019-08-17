0e6093816dbc61060589266d7cf2bec357933aed
blittable <blittable-bits@gmail.com>
2019-08-16 23:12:18 -0600

    merge test

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -2,12 +2,20 @@
 fn main() {
 
     let name = "Nook";
-    let name_with_description = add_description(name); 
+    let name_with_description = add_description(&name); 
+    let result = add();
 
-    println!("Name with description: {:?}" xxxxx);
+    println!("Name with description: {:?}", name_with_description);
+    println!("Name with description: {:?}", result); 
 }
 
-fn add_description(name: xxxxxx) -> xxxxx {
-
-    //add "is great!" to the name 
+fn add_description(name: &str) -> String {
+    [name, " is great"].concat()
 }
+
+
+fn return_something() -> i32 {
+    1
+}
+
+
