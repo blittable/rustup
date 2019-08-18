0480592b41478d06c0357f51116275440014b0e1
Uthen <uthen@mycostech.com>
2019-08-19 05:38:35 +0700

    First homework

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,6 +1,29 @@
-fn main() {
+static MYCOS: &str = "Mycos ";
+static COMPANY: &str = "Company ";
+static FIRSTAPP: &str = "First appeared";
+static CYEAR: i32 = 2008;
 
-    let name = "Nook";
-    let name_with_description = &name; 
+fn main() {
+    //#1
+    println! ("{0}:{1}",FIRSTAPP,first_appeared());
+
+    //#2
+    let c1 = String::from(MYCOS);
+    let company = get_company(c1);
+    prin_message(company);
 }
 
+//#1 
+fn first_appeared() -> i32 { CYEAR } 
+
+//#2
+fn get_company(mut company: String) -> String
+{
+      company.push_str(COMPANY);
+      return company;
+}
+
+//#Other
+fn prin_message(value:String){
+    println!("{}",value);
+}
