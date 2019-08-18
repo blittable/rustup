734094707654319d27a44738b627b96b52ff4d02
Marisa Mahawanchai <kanoon.marisa@gmail.com>
2019-08-19 01:11:12 +0700

    Assignment

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,6 +1,18 @@
 fn main() {
-
     let name = "Nook";
-    let name_with_description = &name; 
+    let name_with_description = set_description(name); 
+    println!("Hello {}" , name_with_description);
+}
+
+fn set_description(name : &str) -> String {
+    // let mut name_to_owned = String::with_capacity(name.len());
+    // for c in name.chars() {
+    //   if c != ' ' {
+    //      name_to_owned.push(c);
+    //   }
+    // }
+
+    let new_owned_string = name.to_owned() + " is Great!";
+    new_owned_string
 }
 
