d3896441084e991d07d403d27a5c8b4a80cc8c59
Mate <patawee@mycostech.com>
2019-08-19 09:38:05 +0700

    Assignment 1

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,6 +1,17 @@
+
+use std::io;
+
 fn main() {
 
-    let name = "Nook";
-    let name_with_description = &name; 
+    println!("How old are you?");
+
+    let mut age = String::new();
+    io::stdin().read_line(&mut age)
+        .expect("Failed to read line");
+    println!("{}",call_age_text(age.trim().parse().unwrap()));
+}
+fn call_age_text(age:i32) -> std::string::String
+{
+    return format!("I'm {} years old.", age+1);
 }
 
