ab0fd07a3c5989276e8c1516850012f2a2e91b66
KevinNel <nel.kevin.147@gmail.com>
2019-08-19 10:10:24 +0700

    Lesson 1 done.

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
 
     let name = "Nook";
-    let name_with_description = add_description(name); 
+    let name_with_description = add_description(name.to_string()); 
 
-    println!("Name with description: {:?}" xxxxx);
+    println!("Name with description: {:?}", name_with_description);
 }
 
-fn add_description(name: xxxxxx) -> xxxxx {
+fn add_description(name: String) -> String {
 
-    //add "is great!" to the name 
+    let mut result: String = name;
+    result.push_str(" is great!");
+    
+    result
 }
