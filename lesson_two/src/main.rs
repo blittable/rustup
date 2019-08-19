d7d01c16b0ce5ac6c0aac3b3f08c6d16cd944bb7
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-08-19 22:18:30 +0700

    Add some code into lesson_two

--- lesson_two/src/main.rs
+++ lesson_two/src/main.rs
@@ -1,7 +1,20 @@
 //Playing with immutable and borrowing
 
 fn main() {
+    let mut x = 10;
+    x = 12;
 
+    println!("{}", x);
 
+    let a = "String A";
+    let b = "String B";
+    let c = &a;
+
+    println!("{}, {}, {}", a, b, c);
+
+    let d = "String D";
+    let e = d.clone();
+
+    println!("{}, {}", d, e);
 }
 
