8c333bebbfe171a15e5638838be73e081d86e858
toom <toom@mycostech.com>
2019-08-19 10:23:47 +0700

    lesson_one homework

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-
     let name = "Nook";
-    let name_with_description = &name; 
+    let name_with_description = add_description(&name);
+
+    println!("\nName with description: {:?}\n", name_with_description);
 }
 
+fn add_description(name: &str) -> String {
+    format!("{} is great", name)
+}
