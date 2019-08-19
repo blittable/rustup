8292a55d2bcca0897ce93fc4fcd065c2fa5e4a61
nook <nookittipon@gmail.com>
2019-08-19 21:50:24 +0700

    try lesson two

--- lesson_two/src/main.rs
+++ lesson_two/src/main.rs
@@ -2,6 +2,39 @@
 
 fn main() {
 
+    let mut nn = 10;
+    nn = 12;
+    println!("nn is {}", nn);
+
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    // integer not clone 
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+
+    //with function 
+    /// first, string
+    let s = String::from("hello"); 
+    takes_ownership(s);
+    // println!("string is  {}", s); error
+
+    /// second, int
+    let num = 5;
+    makes_copy(num);
+    println!("num is {}", num);
 
 }
 
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
