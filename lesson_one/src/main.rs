a4062fd45b9390228fa64550b6157d8ba0c2f831
sillawatkim <sillawat@mycostech.com>
2019-08-19 08:00:57 +0700

    Homework assignment 1

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
 
     let name = "Nook";
-    let name_with_description = &name; 
+    //let name_with_description = &name;
+
+    let result = Add_XIsGreat(name.to_string());
+    println!("{}", result);
 }
+
+fn Add_XIsGreat(who: String) -> String
+{
+    let message = " is great.".to_string();
+    return format!("{} {}", who, message);
+}
+
+
 
