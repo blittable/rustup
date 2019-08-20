9bcae1397fbeabc5d175a1f93f4307a93ff7ea98
Vee <vee.jirapong@gmail.com>
2019-08-20 22:12:16 +0700

    Done lesson_three

--- lesson_three/src/main.rs
+++ lesson_three/src/main.rs
@@ -1,17 +1,43 @@
 //Basics of Traits - Homework
 
 fn main() {
-
-    //Create a struct that has String field
-
-    //Create a trait that returns a String
-
-    //Implement the trait for your struct
-
     //Call it from main()
+    let me: Person = Person {
+        name: "Vee".to_string(),
+    };
 
+    let message = me.say_hi();
+     println!("Me said: {:?}", message);
+
+    let vee = String::from("Vee");
+    println!(
+        "Anonymous said: {:?}",
+        self::Greeting::say_hi(&vee)
+    );
     //Extra Credit:
     // 1) create a String value
     // 2) print the same string value returned from the trait above, but without using a struct, hint ::
+}
 
+//Create a struct that has String field
+struct Person {
+    name: String,
+}
+
+//Create a trait that returns a String
+trait Greeting {
+    fn say_hi(&self) -> String;
+}
+
+impl Greeting for String {
+    fn say_hi(&self) -> String {
+        "Hi!".to_string()
+    }
+}
+
+//Implement the trait for your struct
+impl Greeting for Person {
+    fn say_hi(&self) -> String {
+        "Hi!".to_string()
+    }
 }
