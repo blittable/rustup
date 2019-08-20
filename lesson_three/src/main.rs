9e464730b4c583394b00bd04205c5e43297b46ab
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-08-20 22:02:26 +0700

    homework lesson_three

--- lesson_three/src/main.rs
+++ lesson_three/src/main.rs
@@ -1,14 +1,29 @@
 //Basics of Traits - Homework
 
+//Create a struct that has String field
+struct Footballer {
+    name: String
+}
+
+//Create a trait that returns a String
+trait Kick {
+    fn kick(&self) -> String;
+}
+
+//Implement the trait for your struct
+impl Kick for Footballer {
+    fn kick(&self) -> String {
+        String::from("Kick... Booomm!!!")
+    }
+}
+
 fn main() {
 
-    //Create a struct that has String field
-
-    //Create a trait that returns a String
-
-    //Implement the trait for your struct
-
     //Call it from main()
+    let _footballer = Footballer {
+        name: String::from("Owen")
+    };
+    println!("{}", _footballer.kick());
 
     //Extra Credit:
     // 1) create a String value
