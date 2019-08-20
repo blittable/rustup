b8db19c332bdd8c9041ceafaa060226da3a2b922
aong <natakith@mycostech.com>
2019-08-20 23:03:07 +0700

    Rust Home work3

--- lesson_one/src/main.rs
+++ lesson_one/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
 
-    let name = "Nook";
-    let name_with_description = &name; 
+    let name: &str = "Aong";
+    let con_name: String = contatenate(name);
+    println!("My name is {}", con_name);
 }
 
+fn contatenate(x: &str) -> String {
+    let title: &str = "Mr.";
+    let result: String = format!("{} {}", title, x);
+    return result;
+}
--- lesson_three/src/main.rs
+++ lesson_three/src/main.rs
@@ -14,4 +14,39 @@ fn main() {
     // 1) create a String value
     // 2) print the same string value returned from the trait above, but without using a struct, hint ::
 
+    let samsung: Mobiles = Mobiles {
+        band: "Samsung".to_string(),
+        price: 499.99,
+    };
+
+    let band_text = samsung.show_band();
+    println!("Mobile: {}", band_text);
+
+    //Extra
+    println!("Extra mobile: {}", self::Mobiles::show_band(&samsung));
+    
+    //Extra2
+    let iphone :String = "iPhone".to_string();
+    let iphone_price :f32 = 555.55;
+    println!("Extra2 mobile: {}", self::Mobiles::show_extra_band(iphone, iphone_price))
+}
+
+struct Mobiles {
+    band: String,
+    price: f32,
+}
+
+trait ShowBand {
+    fn show_band(&self) -> String;
+    fn show_extra_band(manual_band: String, manual_price: f32) -> String;
+}
+
+impl ShowBand for Mobiles {
+    fn show_band(&self) -> String {
+        return format!("band {} price {}",self.band, self.price);
+    }
+
+    fn show_extra_band(manual_band: String, manual_price: f32) -> String {
+        return format!("band {} price {}",manual_band, manual_price);;
+    }
 }
