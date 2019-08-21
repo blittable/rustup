82659d6443777a1c6c2cf3fc3c30b67a338987fb
iamkhwan <puttarak555@gmail.com>
2019-08-21 09:11:06 +0700

    Homework

--- lesson_three/src/main.rs
+++ lesson_three/src/main.rs
@@ -1,17 +1,41 @@
 //Basics of Traits - Homework
 
 fn main() {
-
-    //Create a struct that has String field
-
-    //Create a trait that returns a String
-
-    //Implement the trait for your struct
-
     //Call it from main()
+    let khwan: School = School {
+        name: String::from("Mycostech")
+    };
+
+    let msg = khwan.study();
+    println!("Result: {}", msg);
 
     //Extra Credit:
     // 1) create a String value
-    // 2) print the same string value returned from the trait above, but without using a struct, hint ::
+    let extra = "Mycostech 2".to_string();
 
+    // 2) print the same string value returned from the trait above, but without using a struct, hint ::
+    println!(
+        "Result : {}",
+        self::Study::study(&School { name: extra })
+    ); 
+}
+
+//Create a struct that has String field
+struct School {
+    name: String
+}
+
+//Create a trait that returns a String
+trait Study {
+    fn study(&self) -> String; 
+}
+
+//Implement the trait for your struct
+impl Study for School {
+    fn study(&self) -> String {
+        let msg = " Rust Programming";
+        let mut my_new_str = self.name.clone();
+        my_new_str.push_str(msg);
+        my_new_str        
+    }
 }
