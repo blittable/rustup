86ee403f0f91576fe84d1a5c54d8fb97a3b904e9
nook <nookittipon@gmail.com>
2019-08-21 09:50:42 +0700

    finished by copying from others

--- lesson_three/src/main.rs
+++ lesson_three/src/main.rs
@@ -1,17 +1,35 @@
 //Basics of Traits - Homework
 
 fn main() {
-
-    //Create a struct that has String field
-
-    //Create a trait that returns a String
-
-    //Implement the trait for your struct
-
     //Call it from main()
+    let man_u: Club = Club {
+        name: "Man U".to_string(),
+        num_of_player: 33
+    };
 
-    //Extra Credit:
+    let showing = man_u.play();
+    println!("GOoooo, {}", showing);
+    //Noo haha
+    //Extra Credit: 
     // 1) create a String value
     // 2) print the same string value returned from the trait above, but without using a struct, hint ::
 
+}
+
+//Create a struct that has String field
+struct Club {
+    name: String,
+    num_of_player: i32
+}
+
+ //Create a trait that returns a String
+trait Matching {
+    fn play(&self) -> String;
+}
+
+ //Implement the trait for your struct
+impl Matching for Club {
+    fn play(&self) -> String {
+        return format!("Team {} play with {} players", self.name, self.num_of_player)
+    }
 }
