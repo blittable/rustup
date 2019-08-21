f08150451985384246a7e8eb36d69dcac59c9911
krit <krit@mycostech.com>
2019-08-21 10:27:30 +0700

    update homework 3

--- lesson_three/src/main.rs
+++ lesson_three/src/main.rs
@@ -1,4 +1,38 @@
 //Basics of Traits - Homework
+    struct Basketball { 
+        name: &'static str,             
+    }
+
+    trait Player {        
+        fn new(name: &'static str) -> Self;
+        
+        fn name(&self) -> &'static str;        
+
+        fn talk(&self) {           
+        }    
+    }
+
+    impl Basketball {    
+
+        fn take_action(&mut self) {            
+            println!("{} is playing basketball", self.name());
+        }
+    }
+
+    impl Player for Basketball {
+        
+        fn new(name: &'static str) -> Basketball {
+            Basketball { name: name}
+        }
+
+        fn name(&self) -> &'static str {
+            self.name
+        }       
+        
+        fn talk(&self) {            
+            println!("My name is {}", self.name);
+        }
+    }
 
 fn main() {
 
@@ -12,6 +46,10 @@ fn main() {
 
     //Extra Credit:
     // 1) create a String value
-    // 2) print the same string value returned from the trait above, but without using a struct, hint ::
+    // 2) print the same string value returned from the trait above, but without using a struct, hint ::    
 
+    let mut Krit: Basketball = Player::new("Krit");
+ 
+    Krit.talk();
+    Krit.take_action();    
 }
