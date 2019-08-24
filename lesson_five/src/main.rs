e7d56da9f788ad564068c989021008f7c726826a
Marisa Mahawanchai <kanoon.marisa@gmail.com>
2019-08-24 18:20:36 +0700

    Assignment Day 5

--- lesson_five/src/main.rs
+++ lesson_five/src/main.rs
@@ -1,9 +1,38 @@
+// //Homework: There's a simple command-line application in lesson_five that takes one parameter. 
+// //If the parameter is great than 125, the program errors. Use a Result<T, E> to improve the error handling.
 use std::env;
+use std::error;
+use std::fmt;
+
+type Result<T> = std::result::Result<T, AgeError>;
+
+#[derive(Debug, Clone)]
+struct AgeError;
+
+impl fmt::Display for AgeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid age")
+    }
+}
+impl error::Error for AgeError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        // Generic error, underlying cause isn't tracked.
+        None
+    }
+}
+fn validate_age(age: i32) -> Result<bool>{
+    if(age > 125) {
+        Err(AgeError)
+    }
+    else{
+        Ok(true)
+
+    }
+}
 
 // https://doc.rust-lang.org/rust-by-example/std_misc/arg/matching.html
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     match args.len() {
         1 => {
             eprintln!("We need a command line argument!");
@@ -18,20 +47,26 @@ fn main() {
                     return;
                 }
             };
-
-            validate_age(i)
+            let result = validate_age(i);
+            match result{
+                Ok(_i) => {
+                        println!("Your validate age result is correct.");
+                },
+                Err(e) =>{
+                    println!("Error - {}", e);
+                }
+            }
         }
         _ => {
             eprintln!("Error in parameters");
         }
     };
 }
-
-fn validate_age(age: i32) {
-    println!("Checking Age");
-    if age > 125 {
-        panic! {
-            println!("Age cannot be over 125 {:?} and your input age was: ", age)
-        }
-    }
-}
+// fn validate_age(age: i32) {
+//     println!("Checking Age");
+//     if age > 125 {
+//         panic! {
+//             println!("Age cannot be over 125 {:?} and your input age was: ", age)
+//         }
+//     }
+// }
