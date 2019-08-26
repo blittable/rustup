dd476327e2bb1a8ec254658dc95b3e92834e6f88
Marisa Mahawanchai <kanoon.marisa@gmail.com>
2019-08-26 22:59:02 +0700

    Assignment Day 7

--- lesson_seven/src/main.rs
+++ lesson_seven/src/main.rs
@@ -1,9 +1,27 @@
+pub mod employeelib;
+use employeelib::employee_service::{get_employees};
+
+pub mod customerlib;
+use customerlib::customer_service::{get_top_three_customers};
+
 fn main() {
-    println!("Hello, world!");
+    println!("** Get Employee List **");
+    let employees = get_employees();
+    for emp in employees.iter() {
+            println!("Name: {:?}, Age: {:?}", emp.name, emp.age);
+    }
 
-    let a = [1, 2, 3];
+    println!("** Get Customer List **");
+    let (first_customer, second_customer, unknow) = get_top_three_customers();
+    println!("Name: {:?} Address: {:?}", first_customer.name, first_customer.address);
+    println!("Name: {:?} Address: {:?}", second_customer.name, second_customer.address);
+    println!("{:?}", unknow);
+    
+    // println!("Hello, world!");
 
-    a.iter().map(|&x| x * 2);
+    // let a = [1, 2, 3];
 
-    assert_eq!(vec![2, 4, 6], doubled);
+    // a.iter().map(|&x| x * 2);
+
+    // assert_eq!(vec![2, 4, 6], doubled);
 }
