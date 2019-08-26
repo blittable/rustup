670b76d9e53691998b4729d305fd6951e6750a37
sillawatkim <sillawat@mycostech.com>
2019-08-27 03:04:31 +0700

    Added tuple and square mod

--- lesson_seven/src/main.rs
+++ lesson_seven/src/main.rs
@@ -1,9 +1,14 @@
+pub mod squarelib;
+
 fn main() {
     println!("Hello, world!");
 
-    let a = [1, 2, 3];
+    // let a = [1, 2, 3];
 
-    a.iter().map(|&x| x * 2);
+    // a.iter().map(|&x| x * 2);
 
-    assert_eq!(vec![2, 4, 6], doubled);
+    // assert_eq!(vec![2, 4, 6], doubled);
+
+    let (h, w, d) = squarelib::square::generatedimention();
+    println!("[Height:{}; Width:{}; Deep:{}]", h, w, d);
 }
