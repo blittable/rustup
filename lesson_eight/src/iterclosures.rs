cb406cb57f6708997f6efb2ed3c7028c0048a13c
Marisa Mahawanchai <kanoon.marisa@gmail.com>
2019-08-27 22:25:52 +0700

    Assignment Day 8

--- lesson_eight/src/iterclosures.rs
+++ lesson_eight/src/iterclosures.rs
@@ -6,7 +6,7 @@ fn main() {
 
 fn chaining() {
 
- let names = [
+    let names = [
              "Jim".to_string(),
              "Mariam".to_string(),
              "Collette".to_string(),
@@ -23,11 +23,16 @@ fn chaining() {
             "Bob".to_string(),
        ];
         
-        let vector: Vec<String> = names
-            .into_iter()
-            .filter(|x| x.len() < 4)
-            .map(|y| y.to_string())
-            .collect::<Vec<String>>();
-        
-        println!("What's our result? : {:?}", &vector);
+    let add_desciption = |name: &str| "Name is ".to_string() + name;
+    
+    let mut vector: Vec<String> = names
+        .into_iter()
+        .filter(|x| x.len() < 4)
+        .map(|y| -> String { add_desciption(&y).to_string()}) //This code mean 'use two closures in your solution' or not?
+        .collect::<Vec<String>>();
+    
+    vector.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    
+    println!("What's our result? : {:?}", &vector);
+
 }
