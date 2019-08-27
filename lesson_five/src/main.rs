d45d76f0c31b36770137eb15b5afeb99dae71b90
KevinNel <nel.kevin.147@gmail.com>
2019-08-27 08:55:46 +0700

    Lesson 5 done.

--- lesson_five/src/main.rs
+++ lesson_five/src/main.rs
@@ -1,4 +1,33 @@
 use std::env;
+use std::error;
+use std::fmt;
+
+type Result<T> = std::result::Result<T, AgeError>;
+
+#[derive(Debug, Clone)]
+struct AgeError;
+
+impl fmt::Display for AgeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid age.")
+    }
+}
+
+impl error::Error for AgeError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
+
+fn validate_age(age: i32) -> Result<bool> {
+    println!("Checking Age");    
+    if age > 125 {
+        Err(AgeError)
+    }
+    else {
+        Ok(true)
+    }
+}
 
 // https://doc.rust-lang.org/rust-by-example/std_misc/arg/matching.html
 fn main() {
@@ -19,19 +48,14 @@ fn main() {
                 }
             };
 
-            validate_age(i)
+            let result = validate_age(i);
+            match result {
+                Ok(n) => println!("Valid age."),
+                Err(e) => println!("Error: {}", e),
+            }
         }
         _ => {
             eprintln!("Error in parameters");
         }
     };
-}
-
-fn validate_age(age: i32) {
-    println!("Checking Age");
-    if age > 125 {
-        panic! {
-            println!("Age cannot be over 125 {:?} and your input age was: ", age)
-        }
-    }
 }
