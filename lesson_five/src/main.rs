c03b0c786f994a06165a93daa1ea8668073e1c0b
Uthen <uthen@mycostech.com>
2019-08-27 11:14:45 +0700

    Lesson Five: Error Handling - homework

--- lesson_five/src/main.rs
+++ lesson_five/src/main.rs
@@ -1,7 +1,38 @@
+//Lesson Five: Error Handling
+
 use std::env;
+use std::error;
+use std::fmt;
+
+type Result<T> = std::result::Result<T, CustomError>;
+
+#[derive(Debug, Clone)]
+struct CustomError;
+
+fn check_age(result: Result<i32>) {
+    println!("Checking Age...");
+    match result {
+        Ok(n) => println!("Age still on range! {}", n),
+        Err(e) => println!("Show Error: {}", e),
+    }
+}
+
+fn validate(age: i32) -> Result<i32> {    
+    if age > 125 { Err(CustomError) } else { Ok(age) }            
+}
+
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Age error is over!, it's should not over >> 125.")
+    }
+}
+
+impl error::Error for CustomError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> { None }
+}
 
 // https://doc.rust-lang.org/rust-by-example/std_misc/arg/matching.html
-fn main() {
+fn main() {  
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
@@ -18,8 +49,8 @@ fn main() {
                     return;
                 }
             };
-
-            validate_age(i)
+            // Check and validate age.
+            check_age(validate(i));                         
         }
         _ => {
             eprintln!("Error in parameters");
@@ -27,11 +58,3 @@ fn main() {
     };
 }
 
-fn validate_age(age: i32) {
-    println!("Checking Age");
-    if age > 125 {
-        panic! {
-            println!("Age cannot be over 125 {:?} and your input age was: ", age)
-        }
-    }
-}
