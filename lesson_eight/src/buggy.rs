eaacc74933216f32e0c93c17640f3b65948c8918
sillawatkim <sillawat@mycostech.com>
2019-08-28 21:51:02 +0700

    homework lesson eight

--- lesson_eight/src/buggy.rs
+++ lesson_eight/src/buggy.rs
@@ -1,19 +1,36 @@
 #![allow(unused_variables)]
 
 fn main() {
-
-    let nums: [i32; 3] = [1, 2, 3];
-
-    let result = double_some_values(&nums);
-
-    if !result.is_empty()
     {
-        println!("Here are your doubled numbers : {:?}", result)
+        let nums: [i32; 3] = [1, 2, 3];
+
+        let result = double_some_values(&nums);
+
+        if !result.is_empty()
+        {
+            println!("Here are your doubled numbers : {:?}", result)
+        }
+        else
+        {
+            println!("no results were returned")
+        }
     }
-    else
+
     {
-        println!("no results were returned")
+        let nums: [i32; 3] = [1, 2, 2147483647];
+
+        let result = double_some_values(&nums);
+
+        if !result.is_empty()
+        {
+            println!("Here are your doubled numbers : {:?}", result)
+        }
+        else
+        {
+            println!("no results were returned")
+        }
     }
+    
 }
 
 //TODO: This function has a *serious* bug
@@ -21,5 +38,22 @@ fn main() {
 //and return the array with all of the values doubled 
 //So, input => [1,2,3] returns [2,4,6]
 fn double_some_values(nums: &[i32; 3]) -> Vec<i32>  {
-    nums.iter().map(|&n| n * 2).collect()
+    let max_value_at_half :i32 = 1073741823;
+    let min_value_at_half :i32 = -1073741824;
+    
+    let filtered : Vec<i32> = nums.iter()
+        .filter(|&x| x >= &min_value_at_half && x <= &max_value_at_half)
+        .map(|&x| x)
+     .collect();
+    
+    match filtered.iter().count(){
+        3 =>
+        {
+            return filtered.iter().map(|&n| n * 2).collect();
+        }
+        _ =>
+        {
+            return vec![];
+        }
+    }
 }
