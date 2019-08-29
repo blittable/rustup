4cecd28c5e867f69241c7e5a499e52044688183b
sillawatkim <sillawat@mycostech.com>
2019-08-29 17:43:43 +0700

    added lesson ten homework

--- lesson_ten/src/main.rs
+++ lesson_ten/src/main.rs
@@ -9,6 +9,29 @@ fn main() {
 fn higher_math() -> i32 {
     1+2
 }
+#[test]
+fn charge_card_test(){
+    let fat_credit_card: CreditCard = CreditCard {
+        card_number: "54545454".to_string(),
+        available_credit: Some(100.00),
+    };
+
+    let charge_amount :Option<f32> = Some(10.0);
+    let result = charge_card(&fat_credit_card, charge_amount);
+    assert_eq!(result, Some(90.0));
+}
+
+#[test]
+fn charge_card_test_fail(){
+    let fat_credit_card: CreditCard = CreditCard {
+        card_number: "54545454".to_string(),
+        available_credit: Some(100.00),
+    };
+
+    let charge_amount :Option<f32> = Some(10.0);
+    let result = charge_card(&fat_credit_card, charge_amount);
+    assert_eq!(result, Some(80.0));
+}
 
 #[test]
 fn amazing_test() {
@@ -31,4 +54,19 @@ mod tests {
     fn another_amazing_test() {
         assert_eq!(higher_math(), 3);
     }
+}
+
+struct CreditCard{
+    card_number: String,
+    available_credit: Option<f32>,
+}
+
+fn charge_card(card: &CreditCard, charge_amount: Option<f32>) -> Option<f32> {
+    match card.available_credit {
+        None => None,
+        Some(v) => match charge_amount {
+            None => None,
+            Some(a) => Some(v - a),
+        }
+    } 
 }
