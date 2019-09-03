c8a498f3f54cfefec9ab84a2f9798cbd0fa416bb
blittable <blittable-bits@gmail.com>
2019-09-02 23:46:58 -0600

    match up samples for lifetimes &'str

--- demos/joined_types_vec3/src/main.rs
+++ demos/joined_types_vec3/src/main.rs
@@ -5,9 +5,9 @@ enum Regions {
     West,
 }
 
-pub struct Company<'a> {
-    name: &'a str,
-    branches: Vec<Shop>
+pub struct Company {
+    name: String,
+    branches: Vec<Shop>,
 }
 
 pub struct Shop {
@@ -15,8 +15,8 @@ pub struct Shop {
     region: Regions,
 }
 
-impl<'a> Company<'a> {
-    pub fn new(company_name: &'a str) -> Self {
+impl Company {
+    pub fn new(company_name: String) -> Self {
         Self {
             name: company_name,
             branches: Vec::new(),
@@ -28,22 +28,31 @@ impl<'a> Company<'a> {
     }
 }
 
-fn build_company() {
+fn print_shops(company: &Company) {
+    for s in company.branches.iter() {
+        println!("We have shop: {:?}", &s.name);
+    }
+}
 
+fn build_company() -> Company {
     let company_name = "Mycos";
 
-    let mut company = Company::new(company_name);
+    let mut company = Company::new(company_name.to_string());
 
     for i in 0..10 {
         println!("building shop {:?}", i);
-
-        let s = Shop {name: "Mycos".to_string(), region: Regions::North };
-
-
+        let s = Shop {
+            name: ["Mycos ".to_string(), i.to_string()].concat(),
+            region: Regions::North,
+        };
+        company.add_shop(s);
     }
+
+    company
 }
 
 fn main() {
     println!("building shops...");
-    build_company();
+    let c = build_company();
+    print_shops(&c);
 }
