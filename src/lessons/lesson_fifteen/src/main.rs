93aab21a86a5d748388796196e69c97632d3ee79
Vee <vee.jirapong@gmail.com>
2019-09-11 18:20:54 +0700

    Done lesson #15

--- src/lessons/lesson_fifteen/src/main.rs
+++ src/lessons/lesson_fifteen/src/main.rs
@@ -45,46 +45,76 @@ fn main() {
     //house_d is on both streets
     street_1.houses.push(house_d);
     street_0.houses.push(house_d);
+
+    println!("House A\t: Size {:?}", house_a.get_house_size());
+    println!("\t: {:?}", house_a.details());
+    println!("House D\t: Size {:?}", house_d.get_house_size());
+    println!("\t: {:?}", house_d.details());
+    println!("House E\t: Size {:?}", house_e.get_house_size());
+    println!("\t: {:?}", house_e.details());
 }
 
+#[derive(Copy, Clone)]
 struct Room {
-    name: String,
+    name: *mut String,
     room_size: i32,
 }
 
+#[derive(Copy, Clone)]
 struct House {
-    rooms: Vec<Room>,
+    rooms: *mut Vec<Room>,
 }
 
 struct Street {
     houses: Vec<House>,
 }
 
+impl Room {
+    pub fn details(mut self) -> String {
+        let name = unsafe { Box::from_raw(self.name) };
+        let details = format!("{}: {}", name, self.room_size);
+        self.name = Box::into_raw(name);
+        details
+    }
+}
+
 impl House {
     pub fn new() -> House {
-        House { rooms: Vec::new() }
+        House { rooms: Box::into_raw(Box::new(Vec::new())) }
     }
 
-    pub fn get_house_size(&self) -> i32 {
-        self.rooms.iter().map(|x| x.room_size).sum()
+    pub fn get_house_size(mut self) -> i32 {
+        let rooms = unsafe { Box::from_raw(self.rooms) };
+        let size = rooms.iter().map(|x| x.room_size).sum();
+        self.rooms = Box::into_raw(rooms);
+        size
     }
 
     pub fn setup_house(mut self, bedrooms: i8, living_rooms: i8) -> House {
         for i in 0..bedrooms {
             let bedroom: Room = Room {
-                name: format!("bedroom number {}", i),
+                name: Box::into_raw(Box::new(format!("bedroom number {}", i))),
                 room_size: 20,
             };
-            self.rooms.push(bedroom);
+            let mut rooms = unsafe { Box::from_raw(self.rooms) };
+            rooms.push(bedroom);
+            self.rooms = Box::into_raw(rooms);
         }
 
         for i in 0..living_rooms {
             let living_room: Room = Room {
-                name: format!("living room number {}", i),
+                name: Box::into_raw(Box::new(format!("living room number {}", i))),
                 room_size: 34,
             };
-            self.rooms.push(living_room);
+            let mut rooms = unsafe { Box::from_raw(self.rooms) };
+            rooms.push(living_room);
+            self.rooms = Box::into_raw(rooms);
         }
         self
+    }
+
+    pub fn details(&self) -> Vec<String> {
+        let rooms = unsafe { Box::from_raw(self.rooms) };
+        rooms.iter().map(|r| r.details()).collect()
     }
 }
