0f58fef5dd60f80542bf7cb10b06a341a663ecc4
Uthen <uthen@mycostech.com>
2019-09-11 16:51:22 +0700

    home work 12

--- lesson_twelve/src/main.rs
+++ lesson_twelve/src/main.rs
@@ -1,32 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+
+//1: create an inventory item
+//2: output the converted value 
+//3: Use a trait
+
+// 1 kilogram = 2.20462262184878 pounds
+// pounds = 2.2 * kilograms
+
+pub const KILOGRAM_FACTOR: f64 = 1.0;
+pub const KILOGRAM_TONNE_FACTOR: f64 = 14.0;
+pub const KILOGRAM_POUND_FACTOR: f64 = 2.20462262184878;
+pub const KILOGRAM_STONE_FACTOR: f64 = 0.157473;
+
+enum UomWeight { Kilos, Pounds, Stone }
+struct InventoryItem { weight: f64, uom: UomWeight }
+
+pub trait MeasurementConverter {
+    fn to_pounds(&self) -> f64; 
+    fn to_kilos(&self) -> f64;
+    fn to_stone(&self) -> f64;
 }
 
-#[allow(unused_imports)]
-mod test {
-    use std::mem;
+impl MeasurementConverter for InventoryItem {
+    fn to_pounds(&self) -> f64 {
+        match self.uom {
+            UomWeight::Pounds => self.weight, 
+            UomWeight::Kilos => self.weight * KILOGRAM_POUND_FACTOR, 
+            UomWeight::Stone => self.weight * KILOGRAM_TONNE_FACTOR
+        }
+    }   
 
-    //MID-TERM!
-    #[test]
-    fn size_test() {
-        let slice = "C";
-        let word = "Cat";
-        let words = "Cats eat dog food at the sea when the weather is good, but should it turn foul, they turn to the finer indoor delicacies.";
-
-        let slice_size_demo = mem::size_of_val(&slice);
-        println!("slice for \"C\" size: {:?}", &slice_size_demo);
-
-        //mem::size_of_val
-        //signature: pub fn size_of_val<T: ?Sized>(val: &T) -> usize
-        //doc: Returns the size of the pointed-to value in bytes.
-        let slice_size = mem::size_of_val(&slice);
-        let word_size = mem::size_of_val(&word);
-        let sentence_size = mem::size_of_val(&words);
-
-        //Question One: Does this assert pass?
-        assert!(slice_size == word_size); 
-        //Question Two: Does this assert pass?
-        assert!(word_size == sentence_size);
-
+    fn to_kilos(&self) -> f64 {
+        match self.uom {
+            UomWeight::Kilos => self.weight, 
+            UomWeight::Pounds => self.weight / KILOGRAM_POUND_FACTOR, 
+            UomWeight::Stone => self.weight / KILOGRAM_STONE_FACTOR
+        }
     }
+
+    fn to_stone(&self) -> f64 {
+        match self.uom {
+            UomWeight::Stone => self.weight, 
+            UomWeight::Pounds => self.weight / KILOGRAM_TONNE_FACTOR, 
+            UomWeight::Kilos => self.weight * KILOGRAM_STONE_FACTOR
+        }
+    }   
+}
+
+pub fn print_table_header() {    
+    println!("\n{0: <30} | {1: <30} | {2: <30}","Pounds", "Kilos", "Stone");
+}
+
+pub fn display_convert<T>(mass: T) where T: MeasurementConverter {                    
+    println!("{0: <30} | {1: <30} | {2: <30} ", mass.to_pounds(), mass.to_kilos(), mass.to_stone());    
+}
+
+fn main() {     
+    print_table_header();
+    display_convert(InventoryItem {weight: KILOGRAM_FACTOR, uom: UomWeight::Pounds}); 
+    display_convert(InventoryItem {weight: KILOGRAM_FACTOR, uom: UomWeight::Kilos}); 
+    display_convert(InventoryItem {weight: KILOGRAM_FACTOR, uom: UomWeight::Stone});    
 }
