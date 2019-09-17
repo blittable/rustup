f144af08b5cc15d1e96471b04a8ef24450276e4f
Vee <vee.jirapong@gmail.com>
2019-09-17 20:27:23 +0700

    Able to apply multiple filter

--- fs_homework/src/main.rs
+++ fs_homework/src/main.rs
@@ -4,8 +4,7 @@ use std::io::prelude::*;
 
 #[derive(Debug)]
 struct Bitmap<'a> {
-    body: &'a [u8],
-    header: &'a [u8],
+    raw: Vec<u8>,
     content: Vec<u8>,
     name: &'a str
 }
@@ -13,30 +12,24 @@ struct Bitmap<'a> {
 impl<'a> Bitmap<'a> {
     const content_offset: usize = 10;
 
-    fn new(name: &'a str, content: &'a Vec<u8>) -> Self {
+    fn new(name: &'a str, content: Vec<u8>) -> Self {
         Bitmap {
-            header: &content[..14],
-            body: &content[14..],
+            raw: content,
             content: Vec::new(),
             name: name
         }
     }
 
     fn apply(self, filter: Box<dyn Filter>) -> Self {
-        let mut offset = self.header[Bitmap::content_offset] as usize;
-        offset = offset - 14;
-        let mut meta = self.body[..offset].to_vec();
-        let mut translated = filter.apply(&self.body[offset..]);
-        let mut result = self.header.to_vec();
+        let mut offset = self.raw[Bitmap::content_offset] as usize;
+        let mut meta = self.raw[..offset].to_vec();
+        let mut translated = filter.apply(&self.raw[offset..]);
+        let mut result = Vec::new();
+        
         result.append(&mut meta);
         result.append(&mut translated);
-        
-        Bitmap {
-            header: self.header,
-            body: self.body,
-            content: result,
-            name: self.name
-        }
+
+        Self::new(self.name, result)
     }
 }
 
@@ -76,7 +69,7 @@ impl Filter for FlipFilter {
 fn main() {
     let fs = fs::OpenOptions::new();
     let content = &fs::read("bird.bmp").unwrap();
-    let bmp = Bitmap::new("002", &content)
+    let bmp = Bitmap::new("002", content.to_vec())
             .apply(Box::new(InverseColorFilter{}))
             .apply(Box::new(FlipFilter{}));
 
@@ -86,5 +79,5 @@ fn main() {
         .open(format!("{}.bmp", bmp.name))
         .unwrap();
 
-    fp.write_all(&bmp.content);
+    fp.write_all(&bmp.raw);
 }
