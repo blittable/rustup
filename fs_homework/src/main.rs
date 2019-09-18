7b9ae2c412e48af54114751d9e8a7161c46e5bbb
Vee <vee.jirapong@gmail.com>
2019-09-18 11:10:28 +0700

    Do horizontal flip

--- fs_homework/src/main.rs
+++ fs_homework/src/main.rs
@@ -1,29 +1,56 @@
+#![allow(dead_code)]
+#![allow(warnings)]
 use std::fs;
 use std::io;
 use std::io::prelude::*;
+use serde::{Serialize, Deserialize};
 
 #[derive(Debug)]
 struct Bitmap<'a> {
     raw: Vec<u8>,
-    content: Vec<u8>,
+    content: Vec<Vec<(u8, u8, u8)>>,
+    seek: usize,
     name: &'a str
 }
 
 impl<'a> Bitmap<'a> {
     const content_offset: usize = 10;
+    const weight_offset: usize = 18;
 
     fn new(name: &'a str, content: Vec<u8>) -> Self {
+        let weight: i16 = bincode::deserialize(&content[Bitmap::weight_offset..Bitmap::weight_offset+3]).unwrap();
+        let offset: i16 = bincode::deserialize(&content[Bitmap::content_offset..Bitmap::content_offset+3]).unwrap();
+        let seek: usize = offset as usize;
+        let mut data = content[seek..].to_vec();
+
+        let mut rows = Vec::new();
+        let mut columns = Vec::new();
+        for i in 0..data.len()/3 {
+            let start = (i * 3);
+            let len = columns.len() as i16;
+            if len == weight
+            {
+                rows.push(columns);
+                columns = Vec::new();
+            }
+            columns.push((data[start], data[start + 1], data[start + 2]));
+        }
+        rows.push(columns);
+
         Bitmap {
             raw: content,
-            content: Vec::new(),
+            content: rows,
+            seek: seek,
             name: name
         }
     }
 
     fn apply(self, filter: Box<dyn Filter>) -> Self {
-        let mut offset = self.raw[Bitmap::content_offset] as usize;
-        let mut meta = self.raw[..offset].to_vec();
-        let mut translated = filter.apply(&self.raw[offset..]);
+        let mut meta = self.raw[..self.seek].to_vec();
+        let mut translated = filter.apply(self.content)
+                                    .iter()
+                                    .flat_map(|&(b, g, r)| vec![b, g, r])
+                                    .collect::<Vec<_>>();
         let mut result = Vec::new();
         
         result.append(&mut meta);
@@ -35,33 +62,34 @@ impl<'a> Bitmap<'a> {
 
 struct InverseColorFilter {}
 
-struct FlipFilter {}
+enum Flip {
+    Holizontal,
+    Vertical
+}
+struct FlipFilter {
+    flip: Flip
+}
 
 trait Filter {
-    fn apply(&self, data: &[u8]) -> Vec<u8>;
+    fn apply(&self, data: Vec<Vec<(u8, u8, u8)>>) -> Vec<(u8, u8, u8)>;
 }
 
 impl Filter for InverseColorFilter {
-    fn apply(&self, data: &[u8]) -> Vec<u8> {
+    fn apply(&self, data: Vec<Vec<(u8, u8, u8)>>) -> Vec<(u8, u8, u8)> {
         data
             .iter()
-            .map(|&x| !x)
+            .flat_map(|array| array.iter())
+            .map(|&(b, g, r)| (!b, !g, !r))
             .collect::<Vec<_>>()
     }
 }
 
 impl Filter for FlipFilter {
-    fn apply(&self, data: &[u8]) -> Vec<u8> {
-        let mut result = Vec::new();
-        for i in 0..data.len()/3 {
-            let start = (i * 3);
-            result.push((data[start], data[start + 1], data[start + 2]));
-        }
-
-        result
+    fn apply(&self, data: Vec<Vec<(u8, u8, u8)>>) -> Vec<(u8, u8, u8)> {
+        data
             .iter()
-            .rev()
-            .flat_map(|&(a, b, c)| vec![a, b, c])
+            .flat_map(|array| array.iter().rev())
+            .map(|&t| t)
             .collect::<Vec<_>>()
     }
 }
@@ -71,7 +99,9 @@ fn main() {
     let content = &fs::read("bird.bmp").unwrap();
     let bmp = Bitmap::new("002", content.to_vec())
             .apply(Box::new(InverseColorFilter{}))
-            .apply(Box::new(FlipFilter{}));
+            .apply(Box::new(FlipFilter{
+                flip: Flip::Holizontal
+            }));
 
     let mut fp = fs::OpenOptions::new()
         .create(true)
