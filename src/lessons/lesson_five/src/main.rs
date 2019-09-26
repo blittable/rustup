c48aa606bc255f8e87bbbf03b38557cf395a9e2c
blittable <blittable-bits@gmail.com>
2019-09-27 03:47:56 +0700

    sample dyn

--- src/lessons/lesson_five/src/main.rs
+++ src/lessons/lesson_five/src/main.rs
@@ -93,3 +93,132 @@ fn read_config() {
     let decoded: SuiteConfig = toml::from_str(toml_str).unwrap();
     println!("{:#?}", decoded);
 }
+use core::TestConfig;
+use log::info;
+use std::env;
+use std::env::var_os;
+use std::error::Error;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::time;
+
+//use tracing::{instrument, Subscriber} ;
+use tracing::{debug, instrument, Subscriber};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+use tokio::fs;
+
+use core::SuiteConfig;
+
+const DEFAULT_CONFIG: &'static str = "/home/john/test_config.toml";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    info!("hello world!");
+
+    configure_tracing();
+
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let config = build_config(args)?;
+
+    println!("Config Values: {:?}", config.target_dir);
+
+    let resp = run_harness(&config).await?;
+
+    Ok(())
+}
+
+fn configure_tracing() {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter("attrs_basic=trace")
+        .finish();
+
+    tracing::subscriber::with_default(subscriber, || {
+        let cat = 3;
+        tracing::debug!("Debug output from trace");
+    });
+}
+
+#[instrument]
+pub fn build_config(args: Vec<String>) -> Result<SuiteConfig, Box<dyn std::error::Error>> {
+    if args.len() > 0 {
+        println!("Arg 1: {:?}", args[0]);
+
+        let file = OpenOptions::new().open(&args[0]);
+
+        match file {
+            Ok(mut f) => {
+                let mut contents = String::new();
+                f.read_to_string(&mut contents)?;
+                let r: SuiteConfig = toml::from_str(&contents)?;
+
+                return Ok(r);
+            }
+            Err(e) => {
+                println!("Error opening the file: {:?}", e.description());
+                return Err(Box::new(e));
+            }
+        };
+    } else {
+        let file = OpenOptions::new()
+            .read(true)
+            .create(false)
+            .open(DEFAULT_CONFIG);
+
+        match file {
+            Ok(mut f) => {
+                let mut contents = String::new();
+                f.read_to_string(&mut contents)?;
+                let r: SuiteConfig = toml::from_str(&contents)?;
+                return Ok(r);
+            }
+            Err(e) => {
+                println!("Error parsing the configuration file: '\n' description: {:?}, \n raw_os_error: {:?}, \n source: {:?}", e.description(), e.raw_os_error(), e.source());
+                return Err(Box::new(e));
+            }
+        };
+    }
+}
+
+///Simple sequential write
+async fn run_write(config: &TestConfig, tag: i32, content: Vec<u8>) -> Result<(), Box<dyn Error>> {
+    fs::write(
+        "/tmp/load/foo.txt".to_string() + &tag.to_string(),
+        content.as_slice(),
+    )
+    .await?;
+    Ok(())
+}
+
+async fn run_harness(config: &SuiteConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let start = time::Instant::now();
+
+    let mut payload = Vec::new();
+
+    for t in config.tests.iter() {
+        println!("Running Test: {:?}", t);
+    }
+
+    for _ in 0..10000 {
+        payload.push(b'c');
+    }
+
+    // for i in 0..500 {
+    //     let b = payload.clone();
+    //     run_write(i, b).await?;
+    // }
+
+    // let duration = start.elapsed();
+    // println!("Write time: {:?}", duration);
+
+    // for i in 0..500 {
+    //     let b = payload.clone();
+    //     run_write(i, b).await?;
+    // }
+
+    let duration = start.elapsed();
+    println!("F Write time: {:?}", duration);
+
+    Ok(())
+}
