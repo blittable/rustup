0f87f11234faa65a95d8ff7d6c1269d214055a75
blittable <blittable-bits@gmail.com>
2019-09-27 03:46:15 +0700

    add asyc dyn sample

--- src/lessons/lesson_five/src/main.rs
+++ src/lessons/lesson_five/src/main.rs
@@ -35,3 +35,61 @@ fn validate_age(age: i32) {
         }
     }
 }
+pub mod vars;
+
+
+use serde::{Serialize, Deserialize};
+use std::fs;
+use std::io;
+use std::fs::File;
+use std::path::PathBuf;
+use std::str::FromStr;
+use toml::from_str;
+use std::io::ErrorKind;
+use std::ffi::OsString;
+
+
+
+#[derive(Debug, Deserialize)]
+pub struct SuiteConfig {
+    pub target_dir: Option<String>,
+    pub file_size: Option<u64>,
+    pub report_config: Option<ReportConfig>,
+    pub tests: Option<Vec<TestConfig>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ReportConfig {
+    pub output_dir: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TestConfig {
+    pub test: String,
+    pub target_dir: Option<String>,
+    pub iterations: Option<u64>,
+}
+
+pub enum CoreTest { Async_Tokio_Random, Async_Tokio_Sequential, Tokio_Sync_Random, Tokio_Syc_Sequential } 
+
+#[test]
+fn read_config() {
+    let toml_str = r#"
+        target_dir = "/tmp/target"
+        file_size_bytes = 5
+        [report_config]
+        output_dir = "/tmp/reports"
+        [[tests]]
+        test = "async"
+        target_dir = "/tmp/target"
+        iterations = 1
+        [[tests]]
+        test = "bsync"
+        target_dir = "/tmp/target"
+        iterations = 1
+    "#;
+
+    println!("test");
+    let decoded: SuiteConfig = toml::from_str(toml_str).unwrap();
+    println!("{:#?}", decoded);
+}
